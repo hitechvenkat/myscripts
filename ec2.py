1a8539a5564bf2f94307bc781eb8a5c7490f2117
hitechvenkat <68799792+hitechvenkat@users.noreply.github.com>
2020-08-01 09:01:46 +0530

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -1,14 +1,11 @@
 import boto3
 
 region_name = 'us-east-2'
-aws_access_key_id = 'AKIAJMCAY57CKWCUWZJQ'
-aws_secret_access_key = '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'
+aws_access_key_id = 'AKIAJFB73E5C5NH65ZFQ'
+aws_secret_access_key = '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'
 
 client = boto3.client('ec2')
 myec2 = client.describe_instances()
 for listec2 in myec2['Reservations']:
 	for listinsid in listec2['Instances']:
 		print(listinsid['InstanceId'])
-		
-stopec2 = client.stop_instances(
-    InstanceIds=[listinsid['InstanceId']])
