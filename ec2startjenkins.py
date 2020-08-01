1f35693bdae7dd1fc6742847aa267817425284fb
hitechvenkat <68799792+hitechvenkat@users.noreply.github.com>
2020-08-01 09:21:40 +0530

    Update ec2startjenkins.py

--- ec2startjenkins.py
+++ ec2startjenkins.py
@@ -4,7 +4,7 @@ import sys
 region='us-east-2'
 access_key='AKIAJFB73E5C5NH65ZFQ'
 secret_key='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'
-instances='i-009fd4e100940e97f'
+instances=['i-009fd4e100940e97f']
 
 ec2 = boto3.client('ec2',region_name=region,
 	aws_access_key_id=access_key,
